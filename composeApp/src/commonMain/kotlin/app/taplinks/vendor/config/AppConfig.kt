ec4c40326d3c2f9f18c1ed36cef1286fb8d5789a
tamer tamer <amr.dawoodi@hotmail.com>
2025-12-24 14:32:13 +0300

    update appConfig

--- composeApp/src/commonMain/kotlin/app/taplinks/vendor/config/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/app/taplinks/vendor/config/AppConfig.kt
@@ -6,7 +6,7 @@ object AppConfig {
 
     val API_ENDPOINT = if (IS_PROD) "taplink-be.vercel.app" else "staging-taplink-be.vercel.app"
     
-    val API_BASE_URL = if (IS_PROD) "https://taplink-be.vercel.app/api/v1/" else "https://staging-taplink-be.vercel.app/api/v1/"
+    val API_BASE_URL = if (IS_PROD) "https://tapgo-be-api.vercel.app/api/v1/" else "https://staging-taplink-be.vercel.app/api/v1/"
     
     val FRONTEND_BASE_URL = if (IS_PROD) "https://taplinks.app" else "https://taplinks.vercel.app"
 
